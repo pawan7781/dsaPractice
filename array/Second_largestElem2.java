525a632fbb0e4d4d373721efc3f2677b37a049a2
Pawan Magarde <pawanmagarde777@gmail.com>
2024-03-04 18:18:26 +0530

    condition added

--- array/Second_largestElem2.java
+++ array/Second_largestElem2.java
@@ -1,6 +1,6 @@
 public class Second_largestElem2 {
     public static void main(String[] args) {
-        int arr[]={50,60,40,32,70,10};
+        int arr[]={1,8,6,2,5,4,9,3,7};
         int n = arr.length;
      
        int largest=0;
@@ -12,6 +12,10 @@ public class Second_largestElem2 {
                 secondL=largest;
                 largest=arr[i];
                 
+            }
+            else if(arr[i]>secondL)
+            {
+                secondL=arr[i];
             }
         }
         System.out.println(secondL);
