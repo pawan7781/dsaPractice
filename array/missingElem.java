37b35bca0b1a640fa3fcd4656baf250fa12f515c
Pawan Magarde <pawanmagarde777@gmail.com>
2024-03-10 17:47:46 +0530

    missing element lc

--- array/missingElem.java
+++ array/missingElem.java
@@ -1,8 +1,8 @@
 public class missingElem {
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,8,9};
+        int arr[]={1,2,0,3,6,4,5,8,9};
         int n=arr.length;
-        int sum_of_natural_num=(n+1)*(n+2)/2;
+        int sum_of_natural_num=(n+1)*(n)/2;
         int sum=0;
         for(int i =0; i<n; i++)
         {
